3788d3c54d1e7ec5574259c2fc5bad0af4f1e3a6
cijin <cijininurhood@gmail.com>
2021-10-19 21:16:53 +0700

    Feat(CS-001):Add methods for reset and register

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -1,6 +1,9 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { schema, rules } from '@ioc:Adonis/Core/Validator'
 
 import User from 'App/Models/User'
+import PasswordResetEmail from 'App/Mailers/PasswordResetEmail'
+import VerifyEmail from 'App/Mailers/VerifyEmail'
 
 export default class AuthController {
   public async login({ auth, request, response }: HttpContextContract) {
@@ -42,5 +45,153 @@ export default class AuthController {
     await auth.use('api').logout()
 
     return response.json({ message: 'User successfully logged out' })
+  }
+
+  /*
+   * @params: request object with email for the account for password reset
+   *
+   * checks if email is valid
+   * if valid, checks if email present in the db
+   * sends email to the email with a password reset link
+   *
+   */
+  public async sendResetEmail({
+    request,
+    response,
+    logger,
+  }: HttpContextContract) {
+    /*
+     * email validation schema
+     * the schema checks if the request email is valid
+     * and also checks if the email exists is the email column of table users
+     */
+    const emailValidationSchema = schema.create({
+      email: schema.string({}, [
+        rules.email(),
+        rules.exists({
+          table: 'users',
+          column: 'email',
+        }),
+      ]),
+    })
+
+    /*
+     * messages to be returned based on which rules
+     * the validation fails for
+     */
+    const messages = {
+      'email.email': 'Email is not valid',
+      'email.exists': 'No user exists with that email',
+    }
+
+    try {
+      // validate request
+      const { email } = await request.validate({
+        schema: emailValidationSchema,
+        messages,
+      })
+
+      // send email with reset password link
+      new PasswordResetEmail().setEmail(email).createSignedUrl().send()
+
+      return response.json({
+        success: true,
+        message: 'Password reset email sent',
+      })
+    } catch (error) {
+      logger.error(error.message)
+      return response.badRequest(error.messages)
+    }
+  }
+
+  /*
+   * @params context object
+   * @returns User, success boolean, message
+   *
+   * adds a new user if valid to the database
+   *  - validates request to add new user
+   *  - if user valid update model
+   *  - send verification email to new user
+   */
+  public async signUp({
+    request,
+    response,
+    auth,
+    logger,
+  }: HttpContextContract) {
+    /*
+     * create validation schema to check input against
+     *
+     */
+    const newUserSchema = schema.create({
+      username: schema.string({}, [
+        // username must be unique
+        rules.unique({
+          table: 'users',
+          column: 'username',
+        }),
+        rules.minLength(4),
+      ]),
+      email: schema.string({}, [
+        // check if valid email and is unique
+        rules.email(),
+        rules.unique({
+          table: 'users',
+          column: 'email',
+        }),
+      ]),
+      password: schema.string({}, [
+        rules.confirmed('passwordConfirmed'),
+        rules.minLength(8),
+      ]),
+    })
+
+    /*
+     * error messages to show when validation rules fail
+     */
+    const messages = {
+      'required': 'The {{ field }} is required to register an account',
+      'username.unique': 'Username already taken',
+      'email.unique': 'An account with this email already exists',
+      'email.email': 'Email is not valid',
+      'passwordConfirmed.confirmed': 'Passwords do not match',
+    }
+
+    try {
+      // validate inputs
+      const { username, email, password } = await request.validate({
+        schema: newUserSchema,
+        messages,
+      })
+
+      logger.info(`Adding new user to db: ${username}`)
+      const user = await User.create({
+        username,
+        email,
+        password,
+        isEmailVerified: false,
+      })
+
+      logger.info(`Logging in new user: ${user.username}`)
+      /*
+       * create a session for the new user
+       */
+      await auth.use('api').login(user)
+
+      logger.info('Sending email -> verify:email')
+      /*
+       * set username, email and then send verification email to user
+       */
+      await new VerifyEmail()
+        .setUsername(user.username)
+        .setEmail(user.email)
+        .createSignedUrl()
+        .send()
+
+      return response.json({ user, success: true, message: 'User registered' })
+    } catch (error) {
+      logger.error(error)
+      return response.badRequest(error.messages)
+    }
   }
 }

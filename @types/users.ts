21c43bd0c3c6d87194d4c2189b0752e468a6b5d7
cijin <cijininurhood@gmail.com>
2021-10-21 07:44:38 +0700

    Feat(CS-001):Make user type enum not constant

--- @types/users.ts
+++ @types/users.ts
@@ -1,6 +1,6 @@
-export const enum UserTypes {
+export enum UserTypeEnum {
   RESTAURANT = 'Restaurant',
   DINER = 'Diner',
 }
 
-export type UserType = UserTypes.RESTAURANT | UserTypes.DINER
+export type UserType = UserTypeEnum.RESTAURANT | UserTypeEnum.DINER
